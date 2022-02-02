26aac9fa00619e7aa89cf4e9f9d9502e57f149d7
@sumant.singh <sumant.singh@top-doc.com>
2022-02-02 11:42:56 +0530

    script file

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,10 +2,10 @@
 
 cd /home/ubuntu/rapido-bid-admin
 if [ "$DEPLOYMENT_GROUP_NAME" = "stag-admin-code-deploy" ]; then
-  export APPLICATION_ENVIRONMENT=staging
+  export APPLICATION_ENVIRONMENT=admin-staging
   export DOCKER_FILE=docker-compose-stag.yml
 else
-  export APPLICATION_ENVIRONMENT=production
+  export APPLICATION_ENVIRONMENT=admin-prod
   export DOCKER_FILE=docker-compose-prod.yml
 fi
 docker-compose -f $DOCKER_FILE down
